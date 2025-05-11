4dc1ee7562f5350adcf7148fcbf56f235584ab31
wDeloo <wdelo.hack@gmail.com>
2025-05-11 14:21:54 +0200

    sticked to astro component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,10 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
-import react from '@astrojs/react';
-
-// https://astro.build/config
 export default defineConfig({
+  output: 'static',
+  outDir: 'dist',
   vite: {
     plugins: [tailwindcss()]
   },
-
-  integrations: [react()]
 });
